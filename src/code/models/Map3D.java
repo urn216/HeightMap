058db243211c5244848120c9df39fad3cea8fa56
urn216 <willkilty@gmail.com>
2024-02-15 22:21:57 +1300

    0.0.2 - Fixed a bug with vertex ordering in ocean layer of 3D maps

--- src/code/models/Map3D.java
+++ src/code/models/Map3D.java
@@ -25,16 +25,16 @@ public class Map3D extends Model {
   private static Vector3[] generateVerts(float[] heights, int w, int h) {
     Vector3[] res = new Vector3[heights.length+4];
 
-    double oceanHeight = 0.5*Core.MAP_SCALE;
+    double oceanHeight = -0.125*Core.MAP_SCALE;
 
     for (int i = 0; i < res.length-4; i++) {
       res[i] = new Vector3((i%w)-(w/2.0), heights[i]*100*Core.MAP_SCALE, h-1-(i/w)-(h/2.0));
     }
 
-    res[res.length-4] = new Vector3(-w/2.0    , oceanHeight, -h/2.0    );
-    res[res.length-3] = new Vector3( w/2.0 - 1, oceanHeight, -h/2.0    );
-    res[res.length-2] = new Vector3(-w/2.0    , oceanHeight,  h/2.0 - 1);
-    res[res.length-1] = new Vector3( w/2.0 - 1, oceanHeight,  h/2.0 - 1);
+    res[res.length-4] = new Vector3(-w/2.0    , oceanHeight,  h/2.0 - 1);
+    res[res.length-3] = new Vector3( w/2.0 - 1, oceanHeight,  h/2.0 - 1);
+    res[res.length-2] = new Vector3(-w/2.0    , oceanHeight, -h/2.0    );
+    res[res.length-1] = new Vector3( w/2.0 - 1, oceanHeight, -h/2.0    );
 
     return res;
   }
@@ -46,10 +46,10 @@ public class Map3D extends Model {
       res[i] = new Vector2((i%w)/(1.0*w), (i/w)/(1.0*h));
     }
 
-    res[res.length-4] = new Vector2(0.0, 1.0);
-    res[res.length-3] = new Vector2(1.0, 1.0);
-    res[res.length-2] = new Vector2(0.0, 0.0);
-    res[res.length-1] = new Vector2(1.0, 0.0);
+    res[res.length-4] = new Vector2(0.0, 0.0);
+    res[res.length-3] = new Vector2(1.0, 0.0);
+    res[res.length-2] = new Vector2(0.0, 1.0);
+    res[res.length-1] = new Vector2(1.0, 1.0);
 
     return res;
   }
@@ -61,13 +61,13 @@ public class Map3D extends Model {
       for (int x = 0; x < w-1; x++) {
         int a = (x) + (z+1) * w, b = (x+1) + (z+1) * w, c = (x) + (z) * w, d = (x+1) + (z) * w;
         res[2*(x + z * (w-1))]   = new Tri(
-          new Vector3[]{verts[a], verts[d], verts[b]}, 
+          new Vector3[]{verts  [a], verts  [d], verts  [b]}, 
           new Vector2[]{vertUVs[a], vertUVs[d], vertUVs[b]},
           new int[]{a+1, d+1, b+1},
           new int[]{a+1, d+1, b+1}
         );
         res[2*(x + z * (w-1))+1] = new Tri(
-          new Vector3[]{verts[a], verts[c], verts[d]}, 
+          new Vector3[]{verts  [a], verts  [c], verts  [d]}, 
           new Vector2[]{vertUVs[a], vertUVs[c], vertUVs[d]}, 
           new int[]{a+1, c+1, d+1},
           new int[]{a+1, c+1, d+1}
@@ -75,17 +75,19 @@ public class Map3D extends Model {
       }
     }
 
+    int a = verts.length-2, b = verts.length-1, c = verts.length-4, d = verts.length-3;
+
     res[res.length-2] = new Tri(
-      new Vector3[]{verts  [verts  .length-4], verts  [verts  .length-1], verts  [verts  .length-3]}, 
-      new Vector2[]{vertUVs[vertUVs.length-4], vertUVs[vertUVs.length-1], vertUVs[vertUVs.length-3]}, 
-      new int[]{verts  .length-3, verts  .length  , verts  .length-2},
-      new int[]{vertUVs.length-3, vertUVs.length  , vertUVs.length-2}
+      new Vector3[]{verts  [a], verts  [d], verts  [b]}, 
+      new Vector2[]{vertUVs[a], vertUVs[d], vertUVs[b]}, 
+      new int[]{a+1, d+1, b+1},
+      new int[]{a+1, d+1, b+1}
     );
     res[res.length-1] = new Tri(
-      new Vector3[]{verts  [verts  .length-4], verts  [verts  .length-2], verts  [verts  .length-1]}, 
-      new Vector2[]{vertUVs[vertUVs.length-4], vertUVs[vertUVs.length-2], vertUVs[vertUVs.length-1]}, 
-      new int[]{verts  .length-3, verts  .length-1, verts  .length  },
-      new int[]{vertUVs.length-3, vertUVs.length-1, vertUVs.length  }
+      new Vector3[]{verts  [a], verts  [c], verts  [d]}, 
+      new Vector2[]{vertUVs[a], vertUVs[c], vertUVs[d]}, 
+      new int[]{a+1, c+1, d+1},
+      new int[]{a+1, c+1, d+1}
     );
 
     return res;
